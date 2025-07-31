2fd9af40d05a6f945d9138c28026834f7d66f1d3
Nadav <60968305+SwitchQ@users.noreply.github.com>
2025-07-31 13:31:40 +0300

    production build

---
+++ out/_next/static/chunks/2a223334-e507f016e117a0b0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[779],{5036:(L,e,s)=>{s.d(e,{Ply:()=>k});var k="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z"}}]);
