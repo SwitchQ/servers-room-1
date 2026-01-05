a58c5d9b72c528ed399bd1fac3ad2aff2b53f489
Nadav <60968305+SwitchQ@users.noreply.github.com>
2026-01-05 11:03:07 +0200

    fix ecosystem for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,14 @@ module.exports = {
   apps: [
     {
       name: "servers-room-1",
-      script: "npm",
-      args: "start",
-      cwd: "/home/forge/servers.pages.switchq.co.il", // Update this path
+      cwd: "/home/forge/servers.pages.switchq.co.il",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
       env: {
-        NODE_ENV: "production",
-        PORT: 3000,
-      },
-      env_production: {
         NODE_ENV: "production",
         PORT: 3000,
       },
